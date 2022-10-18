9742b90adcf35a027d004dde5dde3263667833f1
Viola <viola.gubaidullina@yandex.ru>
2022-10-18 15:41:16 +0500

    решили задачу в сишарп

---
+++ task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Решение 
+
+string [] array = {"Hello", "2", "world", ":-)"};
+int index = 0;
+int n = array.Length;
+string width = "   ";
+string find = width;
+
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(array [index]);
+        return;
+    }
+    index++;
+}
+
+Console.WriteLine("Такое значение отсутствует");
