bf018aa21eb56690963df6c764b21902a7201ab1
Viola <viola.gubaidullina@yandex.ru>
2022-10-24 21:55:10 +0500

    перерешали задачу в С# по замечаниям преподавателя

--- task/Program.cs
+++ task/Program.cs
@@ -1,19 +1,53 @@
 ﻿// Решение 
 
-string [] array = {"Hello", "2", "world", ":-)"};
-int index = 0;
-int n = array.Length;
-string width = "   ";
-string find = width;
+using static System.Console;
+Write("Введите размер массива, минимальное и максимальное значения через пробел ");
+int[] parameters = GetParams(ReadLine());
+int[] baseArray = GetArrayFromString(parameters[0], parameters[1], parameters[2]);
 
-while (index < n)
+int [] GetParams(string inString);
 {
-    if (array [index] == find)
+    string[] param = inString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[inString.Length];
+    for(int i = 0; i < inString.Length; i++)
     {
-        Console.WriteLine(array [index]);
-        return;
+        result[i] = int.Parse(param[i]);
     }
-    index++;
+    return result;
 }
 
-Console.WriteLine("Такое значение отсутствует");
+int [] GetArrayFromString(int size, int minValue, int maxValue);
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue,maxValue+1);
+        if(res[i] >= 100 && res[i] <= 999)
+        {
+            WriteLine(res[i]);
+        }
+    }
+    return res;
+}
+
+
+
+
+
+// string[] array = { "Hello", "2", "world", ":-)" };
+// int index = 0;
+// int n = array.Length;
+// string width = "   ";
+// string find = width;
+
+// while (index < n)
+// {
+    // if (array[index] == find)
+    // {
+       //  WriteLine(array[index]);
+        // return;
+   // }
+    // index++;
+// }
+
+// WriteLine("Такое значение отсутствует");
