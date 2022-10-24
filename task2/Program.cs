58eebcfda3ce9628d3331c9a9c9d89237350c3f1
Viola <viola.gubaidullina@yandex.ru>
2022-10-24 22:00:35 +0500

    переререшали задачу в новом файле

---
+++ task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Решение 
+
+using static System.Console;
+Write("Введите размер массива, минимальное и максимальное значения через пробел ");
+int[] parameters = GetParams(ReadLine());
+int[] baseArray = GetArrayFromString(parameters[0], parameters[1], parameters[2]);
+
+int [] GetParams(string inString);
+{
+    string[] param = inString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[inString.Length];
+    for(int i = 0; i < inString.Length; i++)
+    {
+        result[i] = int.Parse(param[i]);
+    }
+    return result;
+}
+
+int [] GetArrayFromString(int size, int minValue, int maxValue);
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue,maxValue+1);
+        if(res[i] >= 100 && res[i] <= 999)
+        {
+            WriteLine(res[i]);
+        }
+    }
+    return res;
+}
