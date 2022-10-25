498fd4227772ff412859af316468a9f6b5a7b423
Viola <viola.gubaidullina@yandex.ru>
2022-10-25 17:06:46 +0500

    последняя версия контрольной работы в C#

---
+++ task3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Решение 
+
+using static System.Console;
+Write("Введите размер массива, минимальное и максимальное значения через пробел ");
+int[] parameters = GetParams(ReadLine());
+int[] baseArray = GetArrayFromString(parameters[0], parameters[1], parameters[2]);
+WriteLine(String.Join(" ", baseArray));
+int count = FindSize(baseArray, 100, 999);
+WriteLine(String.Join(count));
+int[] newArray = GetNewArray(count, 100, 999);
+WriteLine(String.Join(" ", newArray));
+
+int [] GetParams(string inStrung)
+{
+    string[] param = inStrung.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[param.Length];
+    for(int i = 0; i < param.Length; i++)
+    {
+        result[i] = int.Parse(param[i]);
+    }
+    return result;
+}
+
+int [] GetArrayFromString(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue,maxValue+1);         
+    }
+    return res;
+}
+
+int [] FindSize(int[] inArray, int left, int right)
+{
+    int resalt = 0;
+    for(int i = 0; i < inArray.Length; i++)
+    {
+        if (inArray[i] >= left && inArray[i] <= right)
+        {
+            resalt++;
+        }
+    }
+    return resalt;
+}
+
+int [] GetNewArray(int count, int left, int right)
+{
+    int[] solution = new int[count];
+    for(int i = 0; i < count; i++)
+    {
+        solution[i] = 
+    }
+    return solution;
+}
